fd9c2a621dfca057ad497ac175fcb2d3c2621f9f
Isaac Tay <IsaacTay@protonmail.com>
2019-12-14 13:06:24 +0800

    Fixing java syntax problems

--- app/app/src/main/java/com/blackcj/customkeyboard/Autocarrot.java
+++ app/app/src/main/java/com/blackcj/customkeyboard/Autocarrot.java
@@ -1,42 +1,44 @@
 package com.blackcj.customkeyboard;
 
 import java.util.List;
-
-public List[String] carrotify(final List[String] suggestions) {
-	for (int i = 0; i < suggestions.length; ++i) {
-		String suggestion = suggestions[i];
-		if (suggestion.length > 4) {
-			StringBuilder sb = new StringBuilder();
-			sb.append(suggestion.charAt(0);
-			sb.append(messUpMessyPart(suggestions[i].substring(1, suggestion.length - 1)));
-			sb.append(suggestion.charAt(suggestion.length - 1));
-			suggestions.set(i, sb.toString());
-		}
-	}
-
-	return suggestions;
-}
-
 import java.util.Random;
 
-Random rand = new Random();
 
-public String messUpMessyPart(String messyPart) {
-	StringBuilder sb = new StringBuider();
-	int a = rand.randInt(0, messyPart.length);
-	int b = rand.randInt(0, messyPart.length - 1);
-	if (a == b) {
-		b += 1;
+public class Utils {
+	Random rand = new Random();
+
+	public List<String> carrotify(final List<String> suggestions) {
+		for (int i = 0; i < suggestions.size(); ++i) {
+			String suggestion = suggestions[i];
+			if (suggestion.length() > 4) {
+				StringBuilder sb = new StringBuilder();
+				sb.append(suggestion.charAt(0);
+				sb.append(messUpMessyPart(suggestions[i].substring(1, suggestion.length() - 1)));
+				sb.append(suggestion.charAt(suggestion.length() - 1));
+				suggestions.set(i, sb.toString());
+			}
+		}
+
+		return suggestions;
 	}
 
-	lb = min(a, b);
-	ub = max(a, b);
+	public String messUpMessyPart(String messyPart) {
+		StringBuilder sb = new StringBuider();
+		int a = rand.randInt(0, messyPart.length());
+		int b = rand.randInt(0, messyPart.length() - 1);
+		if (a == b) {
+			b += 1;
+		}
 
-	sb.append(messyPart.substring(0, a));
-	sb.append(messyPart.charAt(a));
-	sb.append(messyPart.subString(a+1, b));
-	sb.append(messyPart.charAt(b));
-	sb.append(messyPart.subString(b+1));
+		lb = min(a, b);
+		ub = max(a, b);
 
-	return sb.toString();
+		sb.append(messyPart.substring(0, a));
+		sb.append(messyPart.charAt(a));
+		sb.append(messyPart.subString(a+1, b));
+		sb.append(messyPart.charAt(b));
+		sb.append(messyPart.subString(b+1));
+
+		return sb.toString();
+	}
 }
